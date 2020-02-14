8f0474b354bcd174685ca575ea41681ea4759bce
Francois Weber <coach1001@gmail.com>
2020-02-14 15:33:10 +0200

    Mustmatch

--- projects/ng-dui/src/lib/dui-form/form-validators/must-match.validator.ts
+++ projects/ng-dui/src/lib/dui-form/form-validators/must-match.validator.ts
@@ -2,13 +2,15 @@ import { FormGroup, ValidationErrors } from '@angular/forms';
 
 export function MustMatchValidator(controlName: string, metadata: any) {
   return (formGroup: FormGroup): ValidationErrors => {
-    
+ 
     const control = formGroup.controls[controlName];
     const matchingControl = formGroup.controls[metadata.targetField];
 
     if (control.value != matchingControl.value) {
-      control.setErrors({ mustMatch: { field: metadata.targetField } });
-      // control.markAsTouched();
+      control.setErrors({ mustMatch: { field: metadata.targetField } });      
+    } else if (control.errors) {
+      delete control.errors.mustMatch;
+      control.updateValueAndValidity({ onlySelf: true, emitEvent: false });
     }
 
     return null;
