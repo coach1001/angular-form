0a94b622eefc06d763b823199abbe9729d08590d
Francois Weber <francois.weber@tangentsolutions.co.za>
2020-02-17 12:27:12 +0200

    Minor changes

--- src/app/modules/account-dui/components/account-dui-container/account-dui-container.component.ts
+++ src/app/modules/account-dui/components/account-dui-container/account-dui-container.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DuiFlowService } from 'projects/ng-dui/src/lib/dui-flow/services/dui-flow.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-account-dui-container',
   templateUrl: './account-dui-container.component.html',
   styleUrls: ['./account-dui-container.component.scss']
 })
-export class AccountDuiContainerComponent {
+export class AccountDuiContainerComponent implements OnInit {
+
+  currentStep$;
 
   constructor(
     private _fs: DuiFlowService
   ) { }
+
+  ngOnInit() {
+    this.currentStep$ = this._fs.currentStep$;
+  }
 
   back(): void {
     this._fs.backStep();
